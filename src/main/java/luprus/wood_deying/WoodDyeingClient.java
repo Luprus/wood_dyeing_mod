bba7274151042fb3ae132e2675c9c53da4767b54
Luprus <lupuslaurus@gmail.com>
2023-08-09 19:27:45 +0200

    Initial commit

---
+++ src/main/java/luprus/wood_deying/WoodDyeingClient.java
@@ -0,0 +1,9 @@
+package luprus.wood_deying;
+
+import net.fabricmc.api.ClientModInitializer;
+public class WoodDyeingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
