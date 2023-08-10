41150496dca8c0f43d2d94dcb671b580a232d805
Luprus <lupuslaurus@gmail.com>
2023-08-10 10:47:36 +0200

    Adding fibers item, renaming some files and folders.

--- src/main/java/luprus/wood_deying/WoodDyeing.java
+++ src/main/java/luprus/wood_deying/WoodDyeing.java
@@ -1,5 +1,6 @@
 package luprus.wood_deying;
 
+import luprus.wood_deying.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class WoodDyeing implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
